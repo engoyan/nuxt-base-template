e9810ead70b346f79deb2ddf4c2eb41b63a8703d
Vadim Engoyan <vadim@engoyan.com>
2024-07-12 16:02:03 -0700

    init
    cleanup
    
    add asset generator
    
    update to nuxt-base v2
    
    update readme
    
    clanup

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <center-screen>
+    <v-empty-state
+      headline="Welcome!"
+      icon="mdi-emoticon-happy"
+      title="NuxtBasePWA"
+    >
+    </v-empty-state>
+  </center-screen>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "default",
+});
+</script>
